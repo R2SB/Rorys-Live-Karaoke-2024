d7ca0cf1f6c7826756da7d6662b5ddcc7c2ca352
Corey Boyle <corey@kansanian.com>
2025-06-16 19:51:38 +0000

    Improve nav elements

--- roryslivekaroake/src/components/nav/Nav.js
+++ roryslivekaroake/src/components/nav/Nav.js
@@ -54,17 +54,19 @@ const Nav = () => {
             </a>
           </div>
           <div className="nav__icons">
-            <a href="https://www.instagram.com/roryslivekaraoke/" target="__blank" id="nav__ig">
+            <a href="https://www.instagram.com/roryslivekaraoke/" target="__blank" id="nav__ig" className="nav__item">
               <img src={igIcon} className="nav__icon" />
+              <span className="nav__text">Social</span>
             </a>
-            <a href="mailto:karaoke@rorychambers.com?subject=Booking%20Inquiry&amp;body=Please%20include%20the%20following%20details%3A%0A%0ADate%3A%0ALocation%3A%0AEvent%20Type%3A%0ATime%20and%20Length%20of%20Performance%3A%0ANumber%20of%20Guests%3A%0APA%20system%20required%20yes%2Fno%3A%0AWhere%20did%20you%20see%2Fhear%20about%20Rory's%20Live%20Karaoke%3A" target="__blank" id="nav__email">
+            <a href="mailto:karaoke@rorychambers.com?subject=Booking%20Inquiry&amp;body=Please%20include%20the%20following%20details%3A%0A%0ADate%3A%0ALocation%3A%0AEvent%20Type%3A%0ATime%20and%20Length%20of%20Performance%3A%0ANumber%20of%20Guests%3A%0APA%20system%20required%20yes%2Fno%3A%0AWhere%20did%20you%20see%2Fhear%20about%20Rory's%20Live%20Karaoke%3A" target="__blank" id="nav__email" className="nav__item">
               <img src={emailIcon} className="nav__icon"/>
+              <span className="nav__text">Book</span>
             </a>
-            <a href="#" onClick={openVenmo}>
+            <a href="#" onClick={openVenmo} className="nav__item">
               <img src={donateIcon} className="nav__icon" id="donate__icon" />
+              <span className="nav__text">Tip Jar</span>
             </a>
           </div>
-          
         </div>      
     </nav>
   )
